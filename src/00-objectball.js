b6708c0b12501cf29dfad087ae16dc8f83521908
trippl <tlaursen8@yahoo.com>
2024-04-08 21:36:36 -0400

    Object Ball

---
+++ src/00-objectball.js
@@ -0,0 +1,199 @@
+function gameObject() {
+    return {
+        home: {
+            teamName: "Brooklyn Nets",
+            colors: ["Black", "White"],
+            players: {
+                "Alan Anderson": {
+                    number: 0,
+                    shoe: 16,
+                    points: 22,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 3,
+                    blocks: 1,
+                    slamDunks: 1,
+                },
+                "Reggie Evans": {
+                    number: 30,
+                    shoe: 14,
+                    points: 12,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 12,
+                    blocks: 12,
+                    slamDunks: 7,
+                },
+                "Brook Lopez": {
+                    number: 11,
+                    shoe: 17,
+                    points: 17,
+                    rebounds: 19,
+                    assists: 10,
+                    steals: 3,
+                    blocks: 1,
+                    slamDunks: 15,
+                },
+                "Mason Plumlee": {
+                    number: 1,
+                    shoe: 19, 
+                    points: 26,
+                    rebounds: 12,
+                    assists: 6,
+                    steals: 3,
+                    blocks: 8,
+                    slamDunks: 5,
+                },
+                "Jason Terry": {
+                    number: 31,
+                    shoe: 15,
+                    points: 19,
+                    rebounds: 2,
+                    assists: 2,
+                    steals: 4,
+                    blocks: 11,
+                    slamDunks: 1,
+                },
+            },
+        },
+
+        away: {
+            teamName: "Charlotte Hornets",
+            colors:["Turquoise", "Purple"],
+            players:{
+                "Jeff Adrien": {
+                    number: 4,
+                    shoe: 18,
+                    points: 10,
+                    rebounds: 1,
+                    assists: 1,
+                    steals: 2,
+                    blocks: 7,
+                    slamDunks: 2,
+                },
+                "Bismack Biyombo": {
+                    number: 0,
+                    shoe: 16,
+                    points: 12,
+                    rebounds: 4,
+                    assists: 7,
+                    steals: 22,
+                    blocks: 15,
+                    slamDunks: 10,
+                },
+                "DeSagna Diop": {
+                    number: 2,
+                    shoe: 14,
+                    points: 24,
+                    rebounds: 12, 
+                    assists: 12,
+                    steals: 4,
+                    blocks: 5,
+                    slamDunks: 5,
+                },
+                "Ben Gordon": {
+                    number: 8,
+                    shoe: 15,
+                    points: 33,
+                    rebounds: 3,
+                    assists: 2,
+                    steals: 1,
+                    blocks: 1,
+                    slamDunks: 0,
+                },
+                "Brendan Haywood": {
+                    number: 33,
+                    shoe: 15,
+                    points: 6,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 22,
+                    blocks: 5,
+                    slamDunks: 12,
+                },
+            },
+        },
+    }
+}
+console.log(gameObject())
+
+function numPointsScored(playerName) {
+    let gameData = gameObject()
+    for (let team in gameData) {
+        let players = gameData[team].players
+        if (players[playerName]) {
+            return players[playerName].points
+        }
+    }
+    return null
+}
+console.log(numPointsScored("Brendan Haywood"))
+
+ function shoeSize (playerName) {
+    let gameData = gameObject()
+    for (let team in gameData) {
+        let players = gameData[team].players
+        if (players[playerName]) {
+            return players[playerName].shoe
+        }
+    }
+    return null
+ }
+ console.log(shoeSize("Brendan Haywood"))
+
+ function teamColors (teamName) {
+    let gameData = gameObject()
+    for (let team in gameData) {
+        if (gameData[team].teamName === teamName) {
+            return gameData[team].colors
+        }
+    }
+    return null
+ }
+ console.log(teamColors("Charlotte Hornets"))
+
+ function teamNames() {
+    let gameData = gameObject()
+    return [gameData.home.teamName, gameData.away.teamName]
+ }
+ console.log(teamNames())
+
+ function playerNumbers(teamName) {
+    let gameData = gameObject()
+    for (let team in gameData) {
+        if (gameData[team].teamName === teamName) {
+            let players = gameData[team].players
+            return Object.values(players).map(player => player.number)
+        }
+    }
+    return null
+ }
+ console.log(playerNumbers("Brooklyn Nets"))
+
+ function playerStats(playerName) {
+    let gameData = gameObject()
+    for (let team in gameData) {
+        let players = gameData[team].players
+        if (players[playerName]) {
+            return players[playerName]
+        }
+    }
+    return null
+ }
+console.log(playerStats("Brendan Haywood"))
+
+  function bigShoeRebounds() {
+    let gameData = gameObject()
+    let maxShoeSize = 0
+    let playerWithMaxShoe = null
+    for (let team in gameData) {
+        let players = gameData[team].players
+        for (let player in players) {
+            if (players[player].shoe > maxShoeSize) {
+                maxShoeSize = players[player].shoeplayerWithMaxShoe = player
+            }
+        }
+    }
+    return playerWithMaxShoe ? gameData.home.players[playerWithMaxShoe].rebounds : null
+  }
+  console.log(bigShoeRebounds())
